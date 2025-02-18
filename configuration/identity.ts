a29033b9eb58bcbe478495c0369c6844d00b0411
jfadams304 <48414707+jfadams304@users.noreply.github.com>
2025-02-17 19:41:48 -0500

    Update owner description

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Jeff Adams`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Jeff Adams is an MBA candidate at the Kenan-Flagler Business School at UNC.`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
