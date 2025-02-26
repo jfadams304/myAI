19c17cd14c034421aaa5ba2ed17874cbb14e6d22
jfadams304 <48414707+jfadams304@users.noreply.github.com>
2025-02-26 12:51:35 -0500

    Update ui.ts
    Adjusting language to align with new direction

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Jeff Adams' AI Assistant`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI by Jeff Adams`;
-export const CLEAR_BUTTON_TEXT: string = `Clear Chat History`;
-export const PAGE_TITLE: string = `Jeff Adams' AI Assistant`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const CHAT_HEADER: string = `The Sabermetrics Encyclopedia`;
+export const MESSAGE_PLACEHOLDER: string = `Ask me about WAR! Or anything else that comes to mind!`;
+export const FOOTER_MESSAGE: string = `Customized sabermetrics AI by Jeff Adams`;
+export const CLEAR_BUTTON_TEXT: string = `New topic?`;
+export const PAGE_TITLE: string = `The Sabermetrics Encyclopedia`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s sabermetrics AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
